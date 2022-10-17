917e6d99ab424bf852528293e3a5682b22247494
PaiNamratha <115989084+PaiNamratha@users.noreply.github.com>
2022-10-17 16:05:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+a = int(input("Enter a value"))
+b = int(input("Enter b value"))
+def add(a ,b)
+ sum = a+b;
 def print_hi(name):
     print(f'Hi, {name}')  
 
 if __name__ == '__main__':
     print_hi('Namratha Pai')
+    add()
